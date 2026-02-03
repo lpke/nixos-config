397a19b9c56fc4ce2f2f2f754e72140d33ae11ec
Luke <luke@lpdev.io>
2026-02-03 16:23:10 +1100

    add factory function for creating KDE GSync window rules

--- user/plasma/window-rules.nix
+++ user/plasma/window-rules.nix
@@ -4,42 +4,35 @@
 let
   windowDecorations = (import ./apps/windowDecorations.nix).window-rules;
 
+  mkAdaptiveSyncRule = { description, windowClass, matchType ? "exact" }:
+    {
+      description = description;
+      match = {
+        window-class = {
+          value = windowClass;
+          type = matchType;
+        };
+      };
+      apply = {
+        adaptivesync = {
+          value = true;
+          apply = "force";
+        };
+      };
+    };
+
 in
   # merged-in configs:
   windowDecorations ++
 # all other configs:
 [
-  # GSync for Minecraft Beta
-  {
+  (mkAdaptiveSyncRule {
     description = "Minecraft Beta";
-    match = {
-      window-class = {
-        value = "Minecraft Minecraft Beta 1.7.3";
-        type = "exact";
-      };
-    };
-    apply = {
-      adaptivesync = {
-        value = true;
-        apply = "force";
-      };
-    };
-  }
+    windowClass = "Minecraft Minecraft Beta 1.7.3";
+  })
 
-  # GSync for RuneLite
-  {
+  (mkAdaptiveSyncRule {
     description = "RuneLite";
-    match = {
-      window-class = {
-        value = "net-runelite-client-RuneLite";
-        type = "exact";
-      };
-    };
-    apply = {
-      adaptivesync = {
-        value = true;
-        apply = "force";
-      };
-    };
-  }
+    windowClass = "net-runelite-client-RuneLite";
+  })
 ]
